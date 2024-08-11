e5f0b42115da1208dacb4097dfb01871d22bff4d
josehs08 <168867459+josehs08@users.noreply.github.com>
2024-08-11 00:47:59 +0000

    Initial commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Character, Favorites
+import requests
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +38,64 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def list_users():
+    users = User.query.all()
+    response = list(map(lambda x: x.serialize(), users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(response), 200
 
-    return jsonify(response_body), 200
+@app.route('/populate', methods=['GET'])
+def populate_db():
+    response = requests.get('https://www.swapi.tech/api/people?page=1&limit=3')
+    response = response.json()
+    response = response.get('results')
+    for item in response:
+        result = requests.get(item.get('url'))
+        result = result.json()
+        result = result.get('result').get('properties')
+        character = Character()
+        character.height = result.get('height')
+        character.hair_color = result.get('hair_color')
+        character.skin_color = result.get('skin_color')
+        character.eye_color = result.get('eye_color')
+        character.birth_year = result.get('birth_year')
+        character.gender = result.get('gender')
+        character.name = result.get('name')
+        db.session.add(character)
+    try:
+        db.session.commit()
+        return jsonify({"msg": "DB populated successfully"}), 200
+    except Exception as error:
+        print('ERROR:',error)
+        db.session.rollback()
+        return jsonify({"msg": "DB error"}), 500
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = Character.query.all()
+    people_list = list()
+    for people in people:
+        people_list.append(people.serialize())
+    return jsonify(people_list), 200
+
+# POST FAVORITE CHARACTER
+@app.route('/favorite/people/<int:people_id>')
+def add_favorite(people_id):
+    people = Character.query.filter_by(id=people_id).first()
+    if people is None:
+        return jsonify({"msg": "People not found"}), 404
+    favorite = Favorites()
+    favorite.character_id = people_id
+    db.session.add(favorite)
+    try:
+        db.session.commit()
+        return jsonify({"msg": "People added to favorites"}), 201
+    except Exception as error:
+        print('ERROR:',error)
+        db.session.rollback()
+        return jsonify({"msg": "DB error"}), 500
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,11 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    registration_date = db.Column(db.DateTime(), unique=False, nullable=False, default=datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime(), unique=False, nullable=True, default=datetime.now(timezone.utc))
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -14,6 +21,81 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    diameter = db.Column(db.Integer, unique=False, nullable=True)
+    rotation_period = db.Column(db.Float, unique=False, nullable=True)
+    orbital_period = db.Column(db.Float, unique=False, nullable=True)
+    gravity = db.Column(db.Float, unique=False, nullable=True)
+    population = db.Column(db.Integer, unique=False, nullable=True)
+    climate = db.Column(db.String(100), unique=False, nullable=True)
+    terrain = db.Column(db.String(100), unique=False, nullable=True)
+    surface_water = db.Column(db.Float, unique=False, nullable=True)
+    created_at = db.Column(db.DateTime(), unique=False, nullable=True, default=datetime.now(timezone.utc))
+    name = db.Column(db.String(100), unique=False, nullable=True)
+    url = db.Column(db.String(200), unique=False, nullable=True)
+    description = db.Column(db.Text(), unique=False, nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "created_at": self.created_at,
+            "url": self.url,
+            "description": self.description,
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    height = db.Column(db.String(100), unique=False, nullable=True)
+    hair_color = db.Column(db.String(100), unique=False, nullable=True)
+    skin_color = db.Column(db.String(100), unique=False, nullable=True)
+    eye_color = db.Column(db.String(100), unique=False, nullable=True)
+    birth_year = db.Column(db.String(100), unique=False, nullable=True)
+    gender = db.Column(db.String(100), unique=False, nullable=True)
+    created_at = db.Column(db.DateTime(), unique=False, nullable=True, default=datetime.now(timezone.utc))
+    edited_at = db.Column(db.DateTime(), unique=False, nullable=True, default=datetime.now(timezone.utc))
+    name = db.Column(db.String(100), unique=False, nullable=True)
+    homeworld_id = db.Column(db.Integer, db.ForeignKey("planet.id"), unique=False, nullable=True)
+    url = db.Column(db.String(200), unique=False, nullable=True)
+
+    def serialize(self):
+        return  {
+            "name": self.name,
+            "height": self.height,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "created_at": self.created_at,
+            "edited_at": self.edited_at,
+            "url": self.url,
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=False, nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"), unique=False, nullable=True)
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"), unique=False, nullable=True)
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
         }
